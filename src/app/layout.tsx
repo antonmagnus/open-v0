5e89c948292489b4a8893fc8568ed96f9a844a5d
antonmagnus <antma062@student.liu.se>
2024-05-15 12:20:27 +0200

    fix: Update themeColor in metadata and add viewport object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,15 +15,19 @@ export const metadata: Metadata = {
     template: `%s - UI Creator`
   },
   description: 'Create UI components with ease.',
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' }
-  ],
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
     apple: '/apple-touch-icon.png'
   }
+}
+import type { Viewport } from 'next'
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' }
+  ],
 }
 
 interface RootLayoutProps {
