82e20724527a96dd4a67919ccc00aa5f4d9efb39
antonmagnus <antma062@student.liu.se>
2024-09-19 18:08:28 +0200

    feat: Update preview to react 18

--- src/components/live-preview.tsx
+++ src/components/live-preview.tsx
@@ -47,8 +47,8 @@ const LivePreview: React.FC<LivePreviewProps> = ({ className, loading, code, dev
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>React Component Preview</title>
-          <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
-          <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
+          <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+          <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
           <script src="https://cdn.tailwindcss.com"></script>
 
       </head>
